1700ad0f5bbf3558762b1b143f0d291f5e055ba7
HarleyWB <wqq1307890224@163.com>
2018-07-25 17:54:42 +0800

    2018.07.25

--- src/main/java/Test/Solution.java
+++ src/main/java/Test/Solution.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 class ThreadTest {
+
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
         map.put("1", "1");
