33e38444eee3c259ec24fbeac755012fd0f521b2
HarleyWB <wqq1307890224@163.com>
2018-07-26 20:08:26 +0800

    2018.07.26

--- src/main/java/Test/Solution.java
+++ src/main/java/Test/Solution.java
@@ -1,32 +1,31 @@
 package Test;
+
 import com.google.common.util.concurrent.RateLimiter;
 
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
 
-class Rate implements Runnable{
+class Rate implements Runnable {
 
-    public static AtomicInteger integer=new AtomicInteger(0);
-    static private RateLimiter limiter=RateLimiter.create(100);
-    public  void run(){
-        if(limiter.tryAcquire()){
-            System.out.println(Thread.currentThread().getName()+"执行");
+    public static AtomicInteger integer = new AtomicInteger(0);
+    static private RateLimiter limiter = RateLimiter.create(100);
+
+    public void run() {
+        if (limiter.tryAcquire()) {
+            System.out.println(Thread.currentThread().getName() + "执行");
             integer.addAndGet(1);
-        }else {
-            System.out.println(Thread.currentThread().getName()+" 未执行");
+        } else {
+            System.out.println(Thread.currentThread().getName() + " 未执行");
         }
 
     }
 }
 
-class Solution{
+class Solution {
 
     public static void main(String[] args) throws InterruptedException {
 
         for (int i = 0; i < 100; i++) {
-            new  Thread(new Rate()).start();
+            new Thread(new Rate()).start();
         }
         Thread.sleep(3000);
         System.out.println(Rate.integer);
