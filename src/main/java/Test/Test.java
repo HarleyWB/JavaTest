ad5a83a038a517b83ce23a790878e0d6b1bd6f1b
HarleyWB <wqq1307890224@163.com>
2018-08-04 10:26:37 +0800

    2018.08.04

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -1,31 +1,31 @@
 package Test;
 
-class Father {
-    static int i = 0;
+public class Test {
+    public static int partion(int[] a, int low, int high) {
+        int key = a[low];
+        while (low < high) {
+            while (a[high] > key && low < high) {
+                high--;
+            }
+            a[low] = a[high];
+            while (a[low] < key && low < high) {
+                low++;
+            }
+            a[high] = a[low];
 
-    public static void say() {
-        System.out.println("father");
-    }
-
-    public static void say2() {
-
-    }
-
-    public void say3() {
-
-    }
-}
-
-public class Test extends Father {
-
-    public static void say() {
-        System.out.println("test");
-    }
-
-    public static void main(String[] args) throws CloneNotSupportedException {
-
-        Father f = new Test();
-        Father father = new Father();
+        }
+        a[high] = key;
+        return high;
 
     }
+
+    public static void main(String[] args) {
+
+        int[] a = {2, 7, 0, 1, 4};
+        partion(a, 0, a.length - 1);
+        for (int i : a) {
+            System.out.println(i);
+        }
+    }
+
 }
